2be3d0760664a9d2ac74f9308ef294f228b3480c
lesia262 <f.lesenka@gmail.com>
2017-02-03 22:42:07 +0300

    tiki

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,14 +1,25 @@
 class TicTacToe {
     constructor() {
-
+        this.winner = null;
+        this.field = [null, null, null, null, null, null, null, null, null];
+        this.player = 'x';
     }
 
     getCurrentPlayerSymbol() {
-
+        return this.player;
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+        var row, col;
+        row = rowIndex;
+        col = columnIndex;
+        if(this.field[row*3+col] != null) return;
+        this.field[row*3+col] = this.player;
+        if(this.player == 'x') {
+            this.player = 'o';
+        } else {
+            this.player = 'x';
+        }
     }
 
     isFinished() {
@@ -28,7 +39,10 @@ class TicTacToe {
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+    var row, col;
+    row = rowIndex;
+    col = colIndex;
+    return this.field[row*3 + col];
     }
 }
 
